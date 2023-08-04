2461e798c7a40b1ee35b85c1d5f44b6332943696
Thân Văn Hoàng <hoangdklk@gmail.com>
2023-08-04 16:06:43 +0700

    change

--- app/src/main/java/com/example/testselectimage/MainActivity.kt
+++ app/src/main/java/com/example/testselectimage/MainActivity.kt
@@ -4,55 +4,51 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testselectimage.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val pickImageRequestCode = 1
+    private lateinit var pickImageLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // Khởi tạo ActivityResultLauncher với ActivityResultContracts.StartActivityForResult()
+        pickImageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            // Xử lý kết quả chọn ảnh tại đây
+            if (result.resultCode == Activity.RESULT_OK) {
+                // Lấy URI của ảnh đã chọn từ Intent trả về
+                val selectedImageUri = result.data?.data
+                // Hiển thị ảnh đã chọn lên ImageView
+                binding.imageView.setImageURI(selectedImageUri)
+            }
+        }
         binding.buttonPickImage1.setOnClickListener {
             // Tạo Intent yêu cầu lựa chọn ảnh
             val intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type =
                 "image/*" //Đặt kiểu dữ liệu (MIME type) của Intent là "image/*" để chỉ định rằng chỉ tìm kiếm các tệp ảnh.
-            // Gửi yêu cầu chọn ảnh và đặt mã yêu cầu là pickImageRequestCode
-            startActivityForResult(intent, pickImageRequestCode)
+            // Bắt đầu hoạt động chọn ảnh bằng ActivityResultLauncher
+            pickImageLauncher.launch(intent)
         }
 
         binding.buttonPickImage2.setOnClickListener {
             // Tạo Intent yêu cầu lựa chọn ảnh từ Bộ sưu tập hoặc ứng dụng ảnh
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            // Gửi yêu cầu chọn ảnh và đặt mã yêu cầu là pickImageRequestCode
-            startActivityForResult(intent, pickImageRequestCode)
+            // Bắt đầu hoạt động chọn ảnh bằng ActivityResultLauncher
+            pickImageLauncher.launch(intent)
         }
 
         binding.buttonPickImage3.setOnClickListener {
             // Tạo Intent yêu cầu lựa chọn ảnh
             val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
             intent.type = "image/*"
-            // Gửi yêu cầu chọn ảnh và đặt mã yêu cầu là pickImageRequestCode
-            startActivityForResult(intent, pickImageRequestCode)
-        }
-    }
-
-    override fun onActivityResult(
-        requestCode: Int, //requestCode: Mã yêu cầu ban đầu khi gọi startActivityForResult.
-        resultCode: Int,//resultCode: Kết quả của hoạt động con.
-        data: Intent?) //data: Intent chứa dữ liệu trả về từ hoạt động con
-    {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Kiểm tra nếu resultCode trả về thành công và requestCode trả về đúng với mã yêu cầu ban đầu
-        if (resultCode == Activity.RESULT_OK && requestCode == pickImageRequestCode) {
-            // Lấy URI của ảnh đã chọn từ Intent trả về
-            val selectedImageUri = data?.data
-            // Hiển thị ảnh đã chọn lên ImageView
-            binding.imageView.setImageURI(selectedImageUri)
+            // Bắt đầu hoạt động chọn ảnh bằng ActivityResultLauncher
+            pickImageLauncher.launch(intent)
         }
     }
 }
